15df7e22902a42c7c4e727f63e4a1f6b6ff30d1d
Brin Shadewater <Brinshadewater@gmail.com>
2026-02-12 22:03:52 -0800

    logo fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      publicDir: 'public',
       server: {
         port: 3000,
         host: '0.0.0.0',
